ae15d8ecf8d6e345dbd89cfdd504209b2481976e
Faheem <66919065+fahboa@users.noreply.github.com>
2023-08-21 10:17:20 +0100

    Add files via upload
    searching different country weather condition

---
+++ script.js
@@ -0,0 +1,62 @@
+const apiKey = "f6fa5ba4648f0a4e6f6ea002898b9a1f";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather(city) {
+    try {
+        const response = await fetch(`${apiUrl}${city}&appid=${apiKey}`);
+
+        if (response.status === 404) {
+            document.querySelector(".error").style.display = "block";
+            document.querySelector(".weather").style.display = "none";
+        } else {
+            const data = await response.json();
+            console.log(data);
+
+            document.querySelector(".city").innerHTML = data.name;
+            document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+            document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+            document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+            switch (data.weather[0].main) {
+                case "Clouds":
+                    weatherIcon.src = "images/clouds.png";
+                    break;
+                case "Clear":
+                    weatherIcon.src = "images/clear.png";
+                    break;
+                case "Rain":
+                    weatherIcon.src = "images/rain.png";
+                    break;
+                case "Drizzle":
+                    weatherIcon.src = "images/drizzle.png";
+                    break;
+                case "Mist":
+                    weatherIcon.src = "images/mist.png";
+                    break;
+                case "Snow":
+                    weatherIcon.src = "images/snow.png";
+                    break;
+                default:
+                    weatherIcon.src = "images/default.png";
+            }
+
+            document.querySelector(".weather").style.display = "block";
+            document.querySelector(".error").style.display = "none";
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+searchBtn.addEventListener("click", () => {
+    const city = searchBox.value;
+    if (city.trim() !== "") {
+        checkWeather(city);
+    }
+});
+
+checkWeather("Ibadan,Nigeria");
